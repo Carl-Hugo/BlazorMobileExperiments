ddbb55c17988a82e7d1f111af91c77a9d3494eb1
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-06-15 01:30:27 -0400

    Move routes to Constants

---
+++ src/Core/Features/Navs/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Features.Navs
+{
+    public class Constants
+    {
+        public const string Home = "home";
+        public const string Page2 = "page2";
+        public const string Empty = "empty";
+    }
+}
